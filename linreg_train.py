85b82e88c5356cac55f1db9aa416ab6c90249791
ImanSafae <86429366+ImanSafae@users.noreply.github.com>
2024-12-24 13:19:20 +0100

    cleaner code - data needs normalizing

--- linreg_train.py
+++ linreg_train.py
@@ -2,8 +2,6 @@ from utils import *
 
 def estimate_price(mileage, theta_0, theta_1):
     estimated_price = theta_0 + (theta_1 * mileage)
-    print(f"theta0:{theta_0}, theta1: {theta_1}, mileage:{mileage} ")
-    print("price=", estimated_price)
     return estimated_price
 
 def compute_cost(predicted_price, real_price):
@@ -13,12 +11,22 @@ def compute_theta0_gradient(total_costs):
     return np.mean(total_costs)
 
 def compute_theta1_gradient(total_costs, mileage_values):
-    return np.dot(total_costs, mileage_values) / len(mileage_values)
+    return sum(total_costs * mileage_values) / len(mileage_values)
+
+def compute_gradients(mileage_values, price_values, theta_0, theta_1, learning_rate):
+    m = len(mileage_values)
+    predicted_prices = estimate_price(mileage_values, theta_0, theta_1)
+    differences = predicted_prices - price_values
+    
+    tmp_theta0 = learning_rate * (1/m) * np.sum(differences)
+    tmp_theta1 = learning_rate * (1/m) * np.sum(differences * mileage_values)
+    
+    return tmp_theta0, tmp_theta1
 
     
 def linear_regression(dataset):
-    iterations = 36
-    learning_rate = 0.01
+    iterations = 100
+    learning_rate = 0.0001
     theta_0 = 0.0
     theta_1 = 0.0
     columns = dataset.columns
@@ -27,21 +35,18 @@ def linear_regression(dataset):
     print(mileage_values)
 
     for i in range(iterations):
-        total_costs = []
-        for row in range(len(dataset)):
-            data = dataset.loc[row]
-            mileage = mileage_values[row]
-            price = price_values[row]
-            predicted_price = estimate_price(mileage, theta_0, theta_1)
-            cost = compute_cost(predicted_price, price)
-            total_costs.append(cost)
-        theta_0_gradient = compute_theta0_gradient(total_costs)
-        theta_0 -= learning_rate * theta_0_gradient
-        theta_1_gradient = compute_theta1_gradient(total_costs, mileage_values)
-        theta_1 -= learning_rate * theta_1_gradient
-        if (i % 100 == 0):
+        # predicted_prices = estimate_price(mileage_values, theta_0, theta_1)
+        # cost = compute_cost(predicted_prices, price_values)
+        # theta_0_gradient = compute_theta0_gradient(cost)
+        # theta_1_gradient = compute_theta1_gradient(cost, mileage_values)
+        tmp_theta0, tmp_theta1 = compute_gradients(mileage_values, price_values, theta_0, theta_1, learning_rate)
+        theta_0 -= tmp_theta0
+        theta_1 -= tmp_theta1
+
+        if (i % 10 == 0):
             print(f"Iteration {i}: theta_0 = {theta_0}, theta_1 = {theta_1}")
-            print("gradients:", theta_0_gradient, theta_1_gradient)
+            # print("gradients:", theta_0_gradient, theta_1_gradient)
+    print(f"Final values: theta_0 = {theta_0}, theta_1 = {theta_1}")
 
 
 
